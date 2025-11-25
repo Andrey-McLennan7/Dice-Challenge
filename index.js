406536878b017c8aea4b05e9a97c98af981dae87
TheVirtualGamer752 <andreymclennan.work@gmail.com>
2025-11-25 19:30:49 +0000

    Change DOM code to jQuery

--- index.js
+++ index.js
@@ -1,41 +1,26 @@
-function randomNumberGenerator(min, max)
-{
+// Functions
+function random(min, max) {
     return Math.floor((Math.random() * max)) + min;
 }
 
-function changeDice(selectorName, randomNumber)
-{
-    document.querySelector(String(selectorName)).setAttribute("src", "./images/dice" + randomNumber + ".png");
+function changeDice(selectorName, randomNumber) {
+    $(String(selectorName)).attr("src", "./images/dice" + randomNumber + ".png");
 }
 
-function whoWon(heading1, randomNumber1, randomNumber2)
-{
-    if (randomNumber1 > randomNumber2)
-    {
-        // If player 1 wins
-        heading1.innerHTML = "Player 1 Wins!";
-    }
-    else if (randomNumber1 < randomNumber2)
-    {
-        // If player 2 wins
-        heading1.innerHTML = "Player 2 Wins!";
-    }
-    else
-    {
-        // If it is a draw
-        heading1.innerHTML = "Draw!";
-    }
-}
+// Data
+const $h1 = $("h1");
 
-function buttonClicked()
-{
-    let randomNumber1 = randomNumberGenerator(1, 6);
-    let randomNumber2 = randomNumberGenerator(1, 6);
+// Main code
+$("button").on("click", () => {
+    const randomNumber1 = random(1, 6);
+    const randomNumber2 = random(1, 6);
 
     changeDice(".img1", randomNumber1);
     changeDice(".img2", randomNumber2);
 
-    whoWon(document.querySelector("h1"), randomNumber1, randomNumber2);
-}
-
-document.querySelector("button").addEventListener("click", buttonClicked);
+    $h1.html(
+        randomNumber1 > randomNumber2 ? "Player 1 Wins!" :
+        randomNumber1 < randomNumber2 ? "Player 2 Wins!" :
+        "Draw!"
+    );
+});
